791814f64b5ccf8867fe33ecf1a96e0ef1f6cef8
littl <littl@DESKTOP-IK0GM3J.attlocal.net>
2018-05-09 19:44:32 -0700

    Fixed and finished Sort and Search

--- src/NameList.java
+++ src/NameList.java
@@ -1,33 +1,33 @@
 public class NameList
 {
-	private Name[] array;
-    public NameList()
-    {
-        array = new Name[20];
+	private static Name[] array;
+	public NameList()
+	{
+		array = new Name[20];
 		initArray();
 	}
 	public void initArray()
 	{	
-			array[0] = new Name("john", "doe");
-			array[1] = new Name("jane", "doe");
-			array[2] = new Name("michael", "jackson");
-			array[3] = new Name("donald", "trump");
-			array[4] = new Name("hillary", "clinton");
-			array[5] = new Name("bernie", "sanders");
-			array[6] = new Name("barack", "obama");
-			array[7] = new Name("elton", "john");
-			array[8] = new Name("oprah", "winfrey");
-			array[9] = new Name("jeff", "bezos");
-			array[10] = new Name("bill", "gates");
-			array[11] = new Name("spongebob", "squarepants");
-			array[12] = new Name("abraham", "lincoln");
-			array[13] = new Name("elvis", "presley");
-			array[14] = new Name("tiger", "woods");
-			array[15] = new Name("bill", "clinton");
-			array[16] = new Name("michael", "jordan");
-			array[17] = new Name("julius", "ceasar");
-			array[18] = new Name("george", "washington");
-			array[19] = new Name("grigori", "rasputin");
+		array[0] = new Name("john", "doe");
+		array[1] = new Name("jane", "doe");
+		array[2] = new Name("michael", "jackson");
+		array[3] = new Name("donald", "trump");
+		array[4] = new Name("hillary", "clinton");
+		array[5] = new Name("bernie", "sanders");
+		array[6] = new Name("barack", "obama");
+		array[7] = new Name("elton", "john");
+		array[8] = new Name("oprah", "winfrey");
+		array[9] = new Name("jeff", "bezos");
+		array[10] = new Name("bill", "gates");
+		array[11] = new Name("spongebob", "squarepants");
+		array[12] = new Name("abraham", "lincoln");
+		array[13] = new Name("elvis", "presley");
+		array[14] = new Name("tiger", "woods");
+		array[15] = new Name("bill", "clinton");
+		array[16] = new Name("michael", "jordan");
+		array[17] = new Name("julius", "ceasar");
+		array[18] = new Name("george", "washington");
+		array[19] = new Name("grigori", "rasputin");
 	}
 	public void bubbleSort()
 	{  
@@ -46,16 +46,29 @@ public class NameList
 			}
 		}
 	}
-	public int binarySearch(Name name)
+	public static int binarySearch(Name name) 
 	{
-		for(int i = 0; i < array.length; i++)
+		int low = 0;
+		int high = array.length - 1;
+		int mid;
+
+		while (low <= high)
 		{
-			if(array[i].compareTo(name) == 0)
+			mid = (low + high) / 2;
+			if (array[mid].compareTo(name) < 0)
 			{
-				return i;
+				low = mid + 1;
+			} 
+			else if (array[mid].compareTo(name) > 0)
+			{
+				high = mid - 1;	
+			}
+			else 
+			{
+				return mid;
 			}
 		}
-		return -1;            
+		return -1;
 	}
 	@Override
 	public String toString() 
--- src/NameListRunner.java
+++ src/NameListRunner.java
@@ -6,10 +6,10 @@ public class NameListRunner
 		NameList test = new NameList();
 		test.bubbleSort();
 		System.out.print(test + "\n");
-		System.out.println(test.binarySearch(new Name("oprah", "winfrey"))); // returns 18 as it's sorted to the index of 18
-		System.out.println(test.binarySearch(new Name("jane", "doe"))); // returns 4 as it's sorted to the index of 4
-		System.out.println(test.binarySearch(new Name("john", "doe"))); // returns 5 as it's sorted to the index of 5
-		System.out.println(test.binarySearch(new Name("thomas", "eddison"))); //returns -1 b/c it's not in the array
-		System.out.println(test.binarySearch(new Name("charles", "darwin"))); //returns -1 b/c it's not in the array
+		System.out.println(NameList.binarySearch(new Name("oprah", "winfrey"))); // returns 18 as it's sorted to the index of 18
+		System.out.println(NameList.binarySearch(new Name("jane", "doe"))); // returns 4 as it's sorted to the index of 4
+		System.out.println(NameList.binarySearch(new Name("john", "doe"))); // returns 5 as it's sorted to the index of 5
+		System.out.println(NameList.binarySearch(new Name("thomas", "eddison"))); //returns -1 b/c it's not in the array
+		System.out.println(NameList.binarySearch(new Name("charles", "darwin"))); //returns -1 b/c it's not in the array
 	}
 }
